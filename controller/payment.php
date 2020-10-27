15157c8a510b2cc3c243c786e22081b618ff6119
Sabin Shrestha <sabinshrestha15@outlook.com>
2020-10-27 21:38:01 +0545

    payment completed

--- controller/payment.php
+++ controller/payment.php
@@ -16,65 +16,71 @@
         //Turning off autocommit
         mysqli_autocommit($conn, FALSE);
         
-        
-        //Checking Balance of Sender
-        $balance_check_query = "SELECT * FROM account WHERE number='$sender_acc_number'";
-        $result = mysqli_query($conn, $balance_check_query);
-        $customer = mysqli_fetch_assoc($result);
-        $current_balance = 0;
-        if($customer){
-            echo $customer['balance'];
-            $current_balance = $customer['balance'];
+        //check if sender and receiver are different
+        if($sender_acc_number != $receiver_acc_number){
+            //Checking Balance of Sender
+            $balance_check_query = "SELECT * FROM account WHERE number='$sender_acc_number'";
+            $result = mysqli_query($conn, $balance_check_query);
+            $customer = mysqli_fetch_assoc($result);
+            $current_balance = 0;
+            if($customer){
+                echo $customer['balance'];
+                $current_balance = $customer['balance'];
 
-        }else{
-            echo "Invalid Account Number: ";
-        }
+            }else{
+                echo "Invalid Account Number: ";
+            }
 
-        //Checking if the balace is sufficient to initiate the transaction
-        if($current_balance >= $amount){
-            // Proceding Transaction by debiting the acccount and then updating the transaction table
-            
-            //Receiver Account Validation --> Checking if receiver account is valid or not
-            $receiver_acc_check_query = "SELECT * FROM account WHERE number='$receiver_acc_number'  LIMIT 1";
-            $result = mysqli_query($conn, $receiver_acc_check_query);
-            $receiver = mysqli_fetch_assoc($result);
-
-            if ($receiver) { // if Receiver Account exists
+            //Checking if the balace is sufficient to initiate the transaction
+            if($current_balance >= $amount){
+                // Proceding Transaction by debiting the acccount and then updating the transaction table
                 
-                //Debiting the customer account balance
-                $updated_debit_balance = $current_balance - $amount;
-                echo "\n New balance: ".$updated_balance;           
-                $debit_query = "UPDATE account SET balance='$updated_debit_balance' WHERE number='$sender_acc_number'";
-                $trans_update_query="INSERT INTO transaction(sender_acc_number, receiver_acc_number, amount, description,timestamp) VALUES('$sender_acc_number', '$receiver_acc_number', '$amount', '$description','$timestamp')";
-                $credit_query = "UPDATE account SET balance=balance+'$amount' WHERE number='$receiver_acc_number'";
-                try{
-                    $debit_result = mysqli_query($conn,$debit_query);
-                    $credit_result = mysqli_query($conn,$trans_update_query);
-                    $trans_result = mysqli_query($conn,$credit_query);
+                //Receiver Account Validation --> Checking if receiver account is valid or not
+                $receiver_acc_check_query = "SELECT * FROM account WHERE number='$receiver_acc_number'  LIMIT 1";
+                $result = mysqli_query($conn, $receiver_acc_check_query);
+                $receiver = mysqli_fetch_assoc($result);
+
+                if ($receiver) { // if Receiver Account exists
                     
-                    echo "Query Success";
-                    //Now committing the transaction
-                    if(!mysqli_commit($conn)){
-                        echo "Commit  failed.Rolling Back...";
-                        mysqli_rollback($conn);
+                    //Debiting the customer account balance
+                    $updated_debit_balance = $current_balance - $amount;
+                    echo "\n New balance: ".$updated_balance;           
+                    $debit_query = "UPDATE account SET balance='$updated_debit_balance' WHERE number='$sender_acc_number'";
+                    $trans_update_query="INSERT INTO transaction(sender_acc_number, receiver_acc_number, amount, description,timestamp) VALUES('$sender_acc_number', '$receiver_acc_number', '$amount', '$description','$timestamp')";
+                    $credit_query = "UPDATE account SET balance=balance+'$amount' WHERE number='$receiver_acc_number'";
+                    try{
+                        $debit_result = mysqli_query($conn,$debit_query);
+                        $credit_result = mysqli_query($conn,$trans_update_query);
+                        $trans_result = mysqli_query($conn,$credit_query);
                         
-                    }else{  
-                        echo "--- Transaction Successful ---";         
+                        echo "Query Success";
+                        //Now committing the transaction
+                        if(!mysqli_commit($conn)){
+                            echo "Commit  failed.Rolling Back...";
+                            mysqli_rollback($conn);
+                            
+                        }else{  
+                            echo "--- Transaction Successful ---";         
 
+                        }
+
+                    }catch(mysqli_sql_exception $e){
+                        
+                        echo "Error".$e;
                     }
+                        
+                }else
+                    echo 'Receiver Account Does Not Exists.';
+                
+            }else{
+                echo "Insufficient Balance";
+            }
+            mysqli_close($conn);
 
-                }catch(mysqli_sql_exception $e){
-                    
-                    echo "Error".$e;
-                }
-                    
-            }else
-                echo 'Receiver Account Does Not Exists.';
-            
         }else{
-            echo "Insufficient Balance";
+            echo "Sender and Receiver Account Cannot be same.";
         }
-        mysqli_close($conn);
+        
        
 
         
---
+++ main_header.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>iBanking</title>
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>  
+    
+    
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/chartist.js/latest/chartist.min.css">
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <link rel="stylesheet" type="text/css" href="css/util.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <style>
+        .sidebar {
+            position: fixed;
+            top: 0;
+            bottom: 0;
+            left: 0;
+            z-index: 100;
+            padding: 90px 0 0;
+            box-shadow: inset -1px 0 0 rgba(0, 0, 0, .1);
+            z-index: 99;
+        }
+
+        @media (max-width: 767.98px) {
+            .sidebar {
+                top: 11.5rem;
+                padding: 0;
+            }
+        }
+            
+        .navbar {
+            box-shadow: inset 0 -1px 0 rgba(0, 0, 0, .1);
+        }
+
+        @media (min-width: 767.98px) {
+            .navbar {
+                top: 0;
+                position: sticky;
+                z-index: 999;
+            }
+        }
+
+        .sidebar .nav-link {
+            color: #333;
+        }
+
+        .sidebar .nav-link.active {
+            color: #0d6efd;
+        }
+    </style>
+</head>
+<body>
